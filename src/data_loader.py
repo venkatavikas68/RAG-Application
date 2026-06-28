3f8b59f4f2fbfcc71dd8342f70dea0e8ff1b5b0a
venkatavikas68 <132804540+venkatavikas68@users.noreply.github.com>
2026-06-28 11:08:44 +0000

    added module src

---
+++ src/data_loader.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from typing import List, Any
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, CSVLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders.excel import UnstructuredExcelLoader
+from langchain_community.document_loaders import JSONLoader
+
